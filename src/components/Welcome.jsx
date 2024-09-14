0b5c686a89809e10ce1a11d0e4597fd7263f98d1
mujtaba-siddique <mohammad.ghous.mujtabaa@gmail.com>
2024-09-15 00:07:21 +0530

    changes done

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,6 +2,6 @@ import style from "./Welcome.module.css";
 
 const Message = () =>
 {
-    return <p className={style.welcome}> Enjoy Your Day</p>
+    return <p className={style.welcome}> Enjoy Your Day!!</p>
 }
 export default Message;
